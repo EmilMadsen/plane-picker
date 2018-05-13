d0abd041088b304a06119fec32fb8a6536aed588
Emil Madsen <emilmadsen1993@gmail.com>
2018-05-13 10:36:15 +0200

    arrival/departure fix

--- src/components/overview.vue
+++ src/components/overview.vue
@@ -67,7 +67,8 @@
                 times: ['00:00', '01:00', '02:00', '03.00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00' , '20:00', '21:00', '22:00', '23:00'],
                 time: '00:00',
                 date: '',
-                urlBase: 'https://planeapi.herokuapp.com/retrieve',
+                urlBase: 'https://planeapi.herokuapp.com/',
+//                urlBase: 'http://planeapi.local/',
                 type: 'departure',
                 searchTerm: '',
             }
@@ -79,6 +80,7 @@
             {
                 let url = this.urlBase;
 
+                url += this.type;
                 url += '?query=' + this.searchTerm;
                 url += '&date=' + this.formatDate;
                 url += '&time=' + this.formatTime;
